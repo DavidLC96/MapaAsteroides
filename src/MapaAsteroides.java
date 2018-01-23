7e179908d098be3a221e2cc67eec45a2c92616cd
David <lujanero99@gmail.com>
2018-01-23 17:13:05 +0100

    Ejercicio Mapa de Asteroides

---
+++ src/MapaAsteroides.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class MapaAsteroides {
+
+    public static void main (String[] args){
+
+        int casos;
+
+        System.out.print("Introduzca el numero de casos: ");
+        Scanner sc1 = new Scanner(System.in);
+        casos = sc1.nextInt();
+
+        while (casos > 0){
+
+            System.out.print("Introduzca el numero de filas: ");
+            System.out.print("Introduzca el numero de columnas: ");
+
+    }
+
+}
